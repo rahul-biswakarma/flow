2447909ee8d4d4d5338142800a9b4f014fe6b0e7
Rahul Biswakarma <rahul.id39@gmail.com>
2024-06-18 16:43:37 +0530

    fix hover state

--- lib/components/onboarding/project-card.tsx
+++ lib/components/onboarding/project-card.tsx
@@ -1,34 +1,35 @@
 import { Project } from '@prisma/client';
-import { Avatar, Card, Flex, IconButton, Text } from '@radix-ui/themes';
+import { Avatar, Box, Card, Flex, IconButton, Text } from '@radix-ui/themes';
 import { ArrowRightIcon } from '@radix-ui/react-icons';
-import Link from 'next/link';
 
 import styles from './onboarding.module.css';
 
 export const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <Link href={`/${project.slug}`}>
-      <Card className={styles.onboardingProjectCard}>
-        <Flex align="center" gap="2" justify="between">
-          <Flex align="center" gap="2">
-            <Avatar fallback="CN" size="3" src="https://github.com/shadcn.png" />
-            <Flex direction="column" gap="0">
-              <Text style={{}}>{project.name}</Text>
-              <Text
-                style={{
-                  marginTop: '-3px',
-                  color: 'var(--gray-10)',
-                }}
-              >
-                {project.slug}
-              </Text>
+    <Box className={styles.onboardingProjectCard}>
+      <Card asChild>
+        <a href={`/${project.slug}`}>
+          <Flex align="center" gap="2" justify="between">
+            <Flex align="center" gap="2">
+              <Avatar fallback="CN" size="3" src="https://github.com/shadcn.png" />
+              <Flex direction="column" gap="0">
+                <Text style={{}}>{project.name}</Text>
+                <Text
+                  style={{
+                    marginTop: '-3px',
+                    color: 'var(--gray-10)',
+                  }}
+                >
+                  {project.slug}
+                </Text>
+              </Flex>
             </Flex>
+            <IconButton className={styles.projectCardIconButton} size="2" variant="ghost">
+              <ArrowRightIcon />
+            </IconButton>
           </Flex>
-          <IconButton className={styles.projectCardIconButton} size="2" variant="ghost">
-            <ArrowRightIcon />
-          </IconButton>
-        </Flex>
+        </a>
       </Card>
-    </Link>
+    </Box>
   );
 };
