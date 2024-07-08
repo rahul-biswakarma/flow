0cb07cb438cd74c34d3d175e6778d0e3ed906278
Rahul Biswakarma <rahul.id39@gmail.com>
2024-07-08 22:01:05 +0530

    fix: preview

--- libs/types/preview.type.ts
+++ libs/types/preview.type.ts
@@ -2,7 +2,7 @@ import { Button, Box, Text } from '@radix-ui/themes';
 
 export const WebNodeToPreview = (type: string) => {
   switch (type) {
-    case 'system-body-node':
+    case 'system-main-node':
       return Box;
     case 'system-text-node':
       return Text;
