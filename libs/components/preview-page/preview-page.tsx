004a86db98ccf1343a173c7312ffb0451152d5b9
Rahul Biswakarma <rahul.id39@gmail.com>
2024-07-14 17:51:25 +0530

    fix

--- libs/components/preview-page/preview-page.tsx
+++ libs/components/preview-page/preview-page.tsx
@@ -2,8 +2,6 @@
 
 import React, { useRef } from 'react';
 
-import ResizablePanel from '../ui/resizable-panel';
-
 import { Preview } from './preview';
 
 export const PreviewPage = () => {
@@ -11,9 +9,7 @@ export const PreviewPage = () => {
 
   return (
     <div ref={parentRef} style={{ width: '100%', height: '100vh', position: 'relative' }}>
-      <ResizablePanel localStorageKey="previewPanelSize" parentRef={parentRef}>
-        <Preview />
-      </ResizablePanel>
+      <Preview />
     </div>
   );
 };
