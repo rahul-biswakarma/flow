99d16f1b98e55f3e631a5b1222847b9420161a22
Rahul Biswakarma <rahul.id39@gmail.com>
2024-10-19 13:04:51 +0530

    updated project with nextjs 15 and react 19

--- apps/app/src/app/[locale]/(dashboard)/layout.tsx
+++ apps/app/src/app/[locale]/(dashboard)/layout.tsx
@@ -1,10 +1,17 @@
 import { I18nProviderClient } from "@/locales/client";
 import type { ReactElement } from "react";
 
-export default function Layout({
-  params: { locale },
-  children,
-}: { params: { locale: string }; children: ReactElement }) {
-  // This is where your authenticated app lives, add a sidebar, header etc.
+export default async function Layout(props: { params: Promise<{ locale: string }>; children: ReactElement }) {
+  const params = await props.params;
+
+  const {
+    locale
+  } = params;
+
+  const {
+    children
+  } = props;
+
+  // This is where your authenticated app lives
   return <I18nProviderClient locale={locale}>{children}</I18nProviderClient>;
 }
