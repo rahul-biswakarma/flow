11c1e09c124dea337e3be450fa225e352d98190c
Rahul Biswakarma <rahul.id39@gmail.com>
2024-10-24 05:00:51 +0530

    fix lints

--- apps/app/src/app/[locale]/(dashboard)/layout.tsx
+++ apps/app/src/app/[locale]/(dashboard)/layout.tsx
@@ -1,16 +1,15 @@
 import { I18nProviderClient } from "@/locales/client";
 import type { ReactElement } from "react";
 
-export default async function Layout(props: { params: Promise<{ locale: string }>; children: ReactElement }) {
+export default async function Layout(props: {
+  params: Promise<{ locale: string }>;
+  children: ReactElement;
+}) {
   const params = await props.params;
 
-  const {
-    locale
-  } = params;
+  const { locale } = params;
 
-  const {
-    children
-  } = props;
+  const { children } = props;
 
   // This is where your authenticated app lives
   return <I18nProviderClient locale={locale}>{children}</I18nProviderClient>;
