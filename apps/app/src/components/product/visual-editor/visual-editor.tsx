0ee6a75f5e2c5464e4c2fa483c1f82180a79ad3d
Rahul Biswakarma <rahul.id39@gmail.com>
2024-10-29 05:14:04 +0530

    added other packages

--- apps/app/src/components/product/visual-editor/visual-editor.tsx
+++ apps/app/src/components/product/visual-editor/visual-editor.tsx
@@ -1,3 +1,19 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@v1/ui/resizable";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 export const VisualEditor = () => {
-	return <div>Visual Editor</div>
-}
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel defaultSize={25}>One</ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>Two</ResizablePanel>
+      </ResizablePanelGroup>
+    </DndProvider>
+  );
+};
